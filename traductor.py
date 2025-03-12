ddb4127ed322bfcd134d2c15e33123294fc01172
NadalBQ <nadalbq@gmail.com>
2025-03-12 12:37:45 +0100

    Corregidos errores y aumentado scope

--- traductor.py
+++ traductor.py
@@ -2,7 +2,7 @@
 from deep_translator import GoogleTranslator
 import pandas as pd
 import langid as lid
-
+import time
 
 def get_file_text(address:str) -> str:
     with open(address, "r", encoding="utf-8") as text:
@@ -23,19 +23,30 @@ def traducir_texto(texto):
 
 
 def is_english(text: str) -> bool:
-    return lid.classify(text) == "en"
+    lang, num = lid.classify(text)
+    return lang == "en"
 
 
-def traducir(csv_address = "valencia_reviews.csv", nombre_columna = "comments", destino = "en_reviews.txt", i = 0, amount = 300):
+def is_spanish(text: str) -> bool:
+    lang, num = lid.classify(text)
+    return lang == "es"
+
+
+def traducir(csv_address = "valencia_reviews.csv", nombre_columna = "comments", destino = "en_reviews.txt", i = 0, amount = -1):
     csv = pd.read_csv(csv_address)
     comentarios = []
     serie_comentarios = csv[nombre_columna]
     com = i
     top = i + amount
+    count_spanish = 0
+    if amount < 0:
+        amount = len(serie_comentarios)
     while i < len(serie_comentarios):
         comentario = serie_comentarios[i]
         i += 1
         comentario = str(comentario)
+        if is_spanish(comentario):
+            count_spanish += 1
         if is_english(comentario):
             pass
         else:
@@ -49,6 +60,10 @@ def traducir(csv_address = "valencia_reviews.csv", nombre_columna = "comments", 
                 errores.append(i)
         if i == top:
             break
-    return "Hecho\n", "errores: ", errores, "comienzo:", com, "fin:", i
-
-print(traducir(i=0, amount=300))
+    #return "Hecho\n", "errores: ", errores, "comienzo:", com, "fin:", i
+    return com, i, errores, count_spanish
+a = time.time()
+print(traducir())
+b = time.time()
+print("Tiempo de hacer todos:", b-a)
+#print(is_spanish("Hola, buenos días a todos, me llamo Rafael"))
