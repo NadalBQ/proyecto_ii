494b23c6faf1c19de2623c2912a6fca54403d333
NadalBQ <nadalbq@gmail.com>
2025-05-21 10:56:34 +0200

    Limpiado repositorio

--- app_demo/example_model_generator.R
+++
@@ -1,9 +0,0 @@
-# Crear modelo de ejemplo
-modelo <- lm(precio ~ m2 + distrito, data = data.frame(
-  precio = c(150000, 180000, 200000, 130000, 170000, 190000),
-  m2 = c(50, 60, 70, 40, 65, 75),
-  distrito = factor(c("Centro", "Centro", "Norte", "Sur", "Sur", "Norte"))
-))
-
-# Guardar el modelo en la carpeta modelos/
-saveRDS(modelo, "modelos/modelo.rds")
--- app_demo/scripts/generar_pdf.R
+++
@@ -1,8 +0,0 @@
-generar_pdf <- function(prediccion, salida) {
-  rmarkdown::render(
-    input = "reportes/informe.Rmd",
-    output_file = salida,
-    params = list(pred = prediccion),
-    envir = new.env(parent = globalenv())
-  )
-}
